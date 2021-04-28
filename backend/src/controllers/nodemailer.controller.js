2c947778e6cf54fa6169419a71de9e19d90b0d56
vuvanduyT613 <vu.van.duy239@example.com>
2021-04-28 22:46:50 +0700

    update checked gmail

---
+++ backend/src/controllers/nodemailer.controller.js
@@ -0,0 +1,58 @@
+const catchAsync = require('../utils/catchAsync');
+const nodemailer = require('nodemailer');
+const randomNumber = require('random');
+
+const sendMail = catchAsync(async (req, res) => {
+  const { email } = req.body;
+  const randomCode = randomNumber.int((min = 1000), (max = 99999));
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    tls: {
+      rejectUnauthorized: false,
+    },
+    auth: {
+      user: 'cs01052021@gmail.com',
+      pass: 'Capstone123456',
+    },
+  });
+
+  const content = `
+  <p style="padding: 0;
+  font-size: 17px;
+  color: #707070">
+    Medical Schedule account
+  <p>
+  <div>
+    <p>Please use the following security code for the Medical Schedule account ${email}.</p>
+    <h5>Security code: ${randomCode}</h5></p>
+    <p>
+        If you don't recognize the  Medical Schedule account ${email}, you can click here to remove your email address from that account.
+    </p>
+    <hr />
+    <p>Thanks,</p>
+    <P>The Medical Schedule account team<P>
+  </div>
+`;
+  const mainOptions = {
+    from: 'cs01052021@gmail.com',
+    to: email,
+    subject: 'Medical Schedule',
+    text: 'Your text is here',
+    html: content,
+  };
+  transporter.sendMail(mainOptions, function (err, info) {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    } else {
+      console.log('Message sent: ' + info.response);
+      res.send({ status: 'Message sent', code: randomCode });
+    }
+  });
+});
+
+module.exports = {
+  sendMail,
+};
---
+++ backend/src/routes/v1/nodemailer.route.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const { mailerController } = require('../../controllers');
+
+const router = express.Router();
+
+router.post('/send', mailerController.sendMail).get('/');
+
+module.exports = router;
+
+/**
+ * @swagger
+ * tags:
+ *   name: Send Gmail
+ *   description: Send Gmail
+ */
+
+/**
+ * @swagger
+ * /nodemail/send:
+ *   post:
+ *     summary: Send gmail as user
+ *     tags: [Send Gmail]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *             example:
+ *               email: fake@example.com
+ *     responses:
+ *       "201":
+ *         description: Send Gmail
+ *         */
